fd2ec179b3192dadecbd69da36d3599a05752590
Attar74 <m.elattar.dev@gmail.com>
2023-05-24 02:03:21 +0300

    importin components inside app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import AddUser from '../src/components/Users/AddUser'
+import UserList from './components/Users/UserList';
 
 function App() {
+
+  const [usersList, updateUsersList] = useState([])
+
+  const updateUsersListHandler = (userName, userAge) => {
+    updateUsersList((prev) => {
+      return [...prev, {
+        id: Math.random().toString(),
+        name: userName,
+        age: userAge
+      }]
+    })
+  }
   return (
     <div>
-
+      <AddUser onAddUser={ updateUsersListHandler } />
+      <UserList users={usersList} />
     </div>
   );
 }
